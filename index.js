a11bf47eea8987000b5efaf89c444266b2f4af90
Giovanni <nibius9@gmail.com>
2021-09-27 14:32:51 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
 // Write your code here!
-const element = document.createElement("div");
+main.remove();
 
-document.body.append(element);
-const ul = document.createElement("ul");
 
-for (let i = 0; i < 3; i++) {
-  const li = document.createElement("li");
-  li.textContent = (i + 1).toString();
-  ul.append(li);
-}
-
-element.append(ul);
-
-const main = document.getElementById("main");
-main.innerHTML =
-  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
-
-<main id="main">
-  <h1>Poodles!</h1>
-  <h3>
-    An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a
-    post-Frankfurt School Appropriationist Perspective
-  </h3>
-  <p><em>By: Byron Q. Poodle, Esq., BA.</em></p>
-</main>
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
